047cf706da07684b82d0e65bc277b669a033b21c
LucasMW <great-lucas@hotmail.com>
2020-05-07 03:20:23 +0100

    Working

--- ImageGalleryApp/ContentView.swift
+++ ImageGalleryApp/ContentView.swift
@@ -11,11 +11,15 @@ import SDWebImageSwiftUI
 
 let code = UIImage(named: "code")!
 let bf = UIImage(named: "bf")!
+
+
 struct ContentView : View {
     
     var images : [UIImage] = [code, bf, code, bf, code, bf, code, bf ]
     @State var urls : [URL] = []
-    @State private var query = ""
+    @State private var query = "pikachu"
+    @ObservedObject private var model = PageViewModel()
+    
     var body: some View {
         VStack {
             ZStack {
@@ -32,55 +36,68 @@ struct ContentView : View {
                 TextField("??", text: $query) .font(Font.system(size: 15, weight: .medium, design: .serif))
                  .textFieldStyle(RoundedBorderTextFieldStyle())
                 Button("Search", action: {
-                    
                     let api = API()
                     api.search(query: self.query, completion: { (page) in
                         print("return \(page?.photo?.count) itens")
                         for photo in page?.photo ?? [] {
                             let id = photo.id ?? ""
                             print(id)
-                            self.urls.removeAll()
+                            self.model.urls.removeAll()
                             api.getAllImagesForId(id: id) { (sizes) in
                                 print(sizes.count)
                                 let size = sizes.last
                                 let url = size?.source ?? ""
-                                self.urls.append(URL(string: url)!)
+                                self.model.urls.append(URL(string: url)!)
                             }
+                            
                         }
                     }, page: 1)
                     
                     })
             }.padding(.horizontal, 20)
-            GridView(images: images)
+            GridView(model: model)
         }
         
     }
 }
+struct ImageURL : Identifiable {
+    var id: ObjectIdentifier
+    var images : UIImage
+    var url : URL
+}
 struct GridView: View {
     var images : [UIImage] = []
-    var urls : [URL?] = []
-    var rows : Int = 0
-    init(images: [UIImage]){
-        self.images = images
-        rows = images.count/2
+    var urls : [URL] {
+        return model.urls
+    }
+    var rows : Int  {
+        return urls.count/2
+    }
+    @ObservedObject private var model : PageViewModel
+    
+    init(model: PageViewModel){
+        self.model = model
+    }
+    func urlFor(i: Int, j: Int) -> URL {
+        return self.urls[i*2+j]
     }
     var body: some View {
         List {
-            ForEach(0..<rows) {
+            ForEach(0..<rows, id: \.self) {
                 i in
                 HStack(alignment: .center) {
                     ForEach(0..<2) {
                        j in
-                        Image(uiImage: self.images[i*2+j])
-                        .resizable().scaledToFit()
-//                        WebImage(url: URL(string: "https://nokiatech.github.io/heif/content/images/ski_jump_1440x960.heic"))
-//                        // Supports options and context, like `.delayPlaceholder` to show placeholder only when error
-//                        .onSuccess { image, cacheType in
-//                            // Success
-//                        }
-//                        .resizable() // Resizable like SwiftUI.Image, you must use this modifier or the view will use the image bitmap size
-//                        .placeholder(Image(uiImage: self.images[i*2+j]))
-                    }
+//                        Image(uiImage: self.images[i*2+j])
+//                        .resizable().scaledToFit()
+                        WebImage(url: self.urlFor(i: i, j: j))
+                        // Supports options and context, like `.delayPlaceholder` to show placeholder only when error
+                        .onSuccess { image, cacheType in
+                            // Success
+                        }
+                        .resizable() // Resizable like SwiftUI.Image, you must use this modifier or the view will use the image bitmap size
+                        .placeholder(Image(systemName: "star"))
+                    }.scaledToFit()
                 }.scaledToFill()
             }
         }
---
+++ ImageGalleryApp/ImageLoader.swift
@@ -0,0 +1,9 @@
+//
+//  ImageLoader.swift
+//  ImageGalleryApp
+//
+//  Created by Lucas Menezes on 5/5/20.
+//  Copyright © 2020 Lucas Menezes. All rights reserved.
+//
+
+import Foundation
